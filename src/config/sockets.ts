0917efeec7eb930c92e87d3afb48e74303114eb9
Tudor Iovita <iovitatudor21@gmail.com>
2022-12-14 19:21:17 +0200

    allow new domains

--- src/config/sockets.ts
+++ src/config/sockets.ts
@@ -3,6 +3,9 @@ export const CORS = {
   methods: ['GET', 'POST'],
   origin: [
     'http://localhost:8080',
+    'http://localhost:8081',
+    'http://localhost:8082',
+    'http://localhost:3000',
     'http://localhost:63342',
     'https://dashboard.talkearn.app',
     'https://instaservice.io',
