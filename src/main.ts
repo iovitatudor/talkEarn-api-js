20a94991a8e803ba798c3496193c458ab52b5d08
Tudor Iovita <iovitatudor21@gmail.com>
2023-01-23 12:23:25 +0200

    Change cors origin to a real url

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'https://dashboard.talkearn.app',
     allowedHeaders:
       'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization, Access-Control-Allow-Origin',
     methods: 'GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS',
