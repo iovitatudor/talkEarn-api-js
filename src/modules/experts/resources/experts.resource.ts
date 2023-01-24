d8f62201a7342a0eba8ef0c91383598085c7089c
Tudor Iovita <iovitatudor21@gmail.com>
2023-01-24 15:45:46 +0200

    Fix error in expert resource

--- src/modules/experts/resources/experts.resource.ts
+++ src/modules/experts/resources/experts.resource.ts
@@ -29,32 +29,34 @@ export class ExpertsResource {
   public updatedAt: string;
 
   public constructor(expert) {
-    const date = new Date(expert.created_at);
-    const updateDate = new Date(expert.updated_at);
+    if (expert) {
+      const date = new Date(expert.created_at);
+      const updateDate = new Date(expert.updated_at);
 
-    this.id = expert.id;
-    this.categoryId = expert.category_id;
-    this.name = expert.name;
-    this.description = expert.description;
-    this.slug = expert.slug;
-    this.email = expert.email;
-    this.recommended = expert.recommended;
-    this.active = expert.active;
-    this.available = expert.available;
-    this.avatar = expert.avatar ? process.env.BASE_URL + expert.avatar : null;
-    this.video = expert.video ? process.env.BASE_URL + expert.video : null;
-    this.profession = expert.profession;
-    this.region = expert.region;
-    this.language = expert.language;
-    this.experience = expert.experience;
-    this.rating = expert.rating;
-    this.price = expert.price;
-    this.type = expert.type;
-    this.category = new CategoriesResource(expert.category);
-    this.parameters = ParametersValueResource.collect(expert.parameters);
-    this.services = ServiceResource.collect(expert.services);
-    this.createdAt = date.toDateString();
-    this.updatedAt = updateDate.toDateString();
+      this.id = expert.id;
+      this.categoryId = expert.category_id;
+      this.name = expert.name;
+      this.description = expert.description;
+      this.slug = expert.slug;
+      this.email = expert.email;
+      this.recommended = expert.recommended;
+      this.active = expert.active;
+      this.available = expert.available;
+      this.avatar = expert.avatar ? process.env.BASE_URL + expert.avatar : null;
+      this.video = expert.video ? process.env.BASE_URL + expert.video : null;
+      this.profession = expert.profession;
+      this.region = expert.region;
+      this.language = expert.language;
+      this.experience = expert.experience;
+      this.rating = expert.rating;
+      this.price = expert.price;
+      this.type = expert.type;
+      this.category = new CategoriesResource(expert.category);
+      this.parameters = ParametersValueResource.collect(expert.parameters);
+      this.services = ServiceResource.collect(expert.services);
+      this.createdAt = date.toDateString();
+      this.updatedAt = updateDate.toDateString();
+    }
   }
 
   public static collect(model: Model[], meta: Record<any, any>) {
